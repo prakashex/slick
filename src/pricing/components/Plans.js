a3b8e03c8225299e29f6c1ebd7a91f84ba038eb1
Prakash Kumar <prakashope@gmail.com>
2023-06-10 16:00:37 +0530

    plans page slider and design

--- src/pricing/components/Plans.js
+++ src/pricing/components/Plans.js
@@ -1,1 +1,35 @@
-export default function Plans() {}
+export default function Plans({ plans }) {
+  const plan = plans[0];
+
+  return (
+    <div className="bg-salmon border-right">
+      <div className="column-padding centered">
+        <div className="callout-wrap">
+          <div className="plan">
+            <div className="plan-wrap">
+              <div className="plan-content">
+                <div className="plan-switch">
+                  Monthly
+                  <label className="switch">
+                    <input type="checkbox" />
+                    <span className="slider" />
+                  </label>
+                  Yearly
+                </div>
+                <h2 className="plan-name">{plan.name}</h2>
+                <div>
+                  Just Rs {plan.price} / {plan.interval}
+                </div>
+                <div>
+                  <button className="large-button">
+                    <div className="large-button-text">Buy Now</div>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
