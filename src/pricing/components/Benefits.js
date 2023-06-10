0f5acc9fad57fb8017d27cdbffba4c8afff30f2c
Prakash Kumar <prakashope@gmail.com>
2023-06-10 15:47:37 +0530

    stripe integration and pricing page

--- src/pricing/components/Benefits.js
+++ src/pricing/components/Benefits.js
@@ -1,1 +1,36 @@
-export default function Benefits() {}
+export default function Benefits() {
+  const benefits = [
+    {
+      title: "One low price",
+      subtitle:
+        "Save big. Get everything with a super low monthly subscription.",
+    },
+    {
+      title: "No limits",
+      subtitle: "Get complete access to everything on the site",
+    },
+    {
+      title: "Cancel anytime",
+      subtitle: "Pause or stop your subscription",
+    },
+  ];
+  return (
+    <div className="bg-white">
+      <div className="column-padding">
+        <div className="content-grid xl">
+          {benefits.map((benifit) => (
+            <div className="spacing-base">
+              <h3 className="font-extrabold text-4xl">
+                {benifit.title }
+                <br />
+              </h3>
+              <div className="">
+                {benifit.subtitle}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
