34b6657bf7a2984ef1372405f75abbc69bf0fa05
Prakash Kumar <prakashope@gmail.com>
2023-06-10 18:10:14 +0530

    success page

--- src/core/layouts/App.js
+++ src/core/layouts/App.js
@@ -1,12 +1,19 @@
+import { useRouter } from "next/router";
 import Meta from "../components/Meta";
 import Navbar from "../components/Navbar";
 
-export default function AppLayout({children}) {
-    return (
-        <>
-            <Meta /> 
-            <Navbar />
-            {children}
-        </>
-    )
+const hideNavbarPages = ["/success"];
+
+export default function AppLayout({ children }) {
+  const router = useRouter();
+  const hideNavbar = hideNavbarPages.includes(router.asPath);
+  console.log("hidenavbar -->", hideNavbar);
+
+  return (
+    <>
+      <Meta />
+      {hideNavbar ? null : <Navbar />}
+      {children}
+    </>
+  );
 }
--- src/pages/success.js
+++ src/pages/success.js
@@ -1,1 +1,27 @@
-export default function SuccessPage() {}
+import Image from "next/image";
+import Link from "next/link";
+import confetti from "../../public/assets/confetti.png"
+export default function SuccessPage() {
+    return (
+        <div className="section bg-pink h-screen">
+                <div className="">
+                    <div className="section-intro welcome">
+                        <Image
+                            src={confetti}
+                            height={200}
+                            widht={200}
+                            alt="confetti"
+                            className="confetti"
+                        />
+                        <h1>You're  In!</h1>
+                        <p>You can now access everything on this site. <br /> Ready to get started?</p>
+                        <Link href="/login" className="large-button" >
+                            <div className="large-button-text">
+                                Get Started
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+        </div>
+    )
+}
