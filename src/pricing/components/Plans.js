3f29b31d78aabaa08e73c0aee8af6e9f6b2a3cdb
Prakash Kumar <prakashope@gmail.com>
2023-06-10 17:36:06 +0530

    stripe session redirect , and api routes

--- src/pricing/components/Plans.js
+++ src/pricing/components/Plans.js
@@ -1,13 +1,25 @@
 import { useState } from "react";
+import { SITE_URL } from "../utils";
+import { loadStripe } from "@stripe/stripe-js";
 
 export default function Plans({ plans }) {
-  const [selected, setSelected] = useState("year");
+  const [selected, setSelected] = useState("month");
   const plan = plans.find((plan) => plan.interval === selected);
+  const [isRedirecting, setIsRedirecting] = useState(false)
 
   const togglePlan = () => {
     const interval = selected === "year" ? "month" : "year";
     console.log(setSelected(interval));
   };
+
+  async function onCheckout(){
+    setIsRedirecting(true)
+    const response = await fetch(`${SITE_URL}/api/checkout/${plan.id}`)
+    const data = await response.json()
+    const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
+    await stripe.redirectToCheckout({sessionId: data.id})
+    setIsRedirecting(false)
+}
 
   return (
     <div className="bg-salmon border-right">
@@ -29,8 +41,8 @@ export default function Plans({ plans }) {
                   Just Rs {plan.price} / {plan.interval}
                 </div>
                 <div>
-                  <button className="large-button">
-                    <div className="large-button-text">Buy Now</div>
+                  <button disabled={isRedirecting} className="large-button">
+                    <div onClick={onCheckout} className="large-button-text">{ isRedirecting ? `Loading...` : `Buy Now`}</div>
                   </button>
                 </div>
               </div>
