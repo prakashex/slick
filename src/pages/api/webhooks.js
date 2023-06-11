0c261bbdfff3ef9161f6a16e24717d3730f34716
Prakash Kumar <prakashope@gmail.com>
2023-06-11 20:41:00 +0530

    creating a new user from payment info

--- src/pages/api/webhooks.js
+++ src/pages/api/webhooks.js
@@ -1,25 +1,76 @@
 import getRawBody from "raw-body";
 import { stripe } from "src/pricing/utils/stripe";
+import { supabase } from "supabase";
 export const config = {
-    api: {
-        bodyParser: false,
-    },
+  api: {
+    bodyParser: false,
+  },
 };
 export default async function handler(req, res) {
   const signature = req.headers["stripe-signature"];
   const signingSecret = process.env.STRIPE_SIGNING_SECRET;
-  console.log("webhooks function ------>")
   let event;
 
   try {
-    const rawBody = await getRawBody(req, {limit: "2mb"});
+    const rawBody = await getRawBody(req, { limit: "2mb" });
     event = stripe.webhooks.constructEvent(rawBody, signature, signingSecret);
   } catch (error) {
     console.log("Webhook signature verification failed.");
     return res.status(400).end();
   }
 
-  console.log("event ---->", event);
-
-  res.send({success: true})
+  try {
+    res.send({ success: true });
+    switch (event.type) {
+      case "customer.subscription.updated":
+        await updateSubscription(event);
+        break;
+      case "customer.subscripton.deleted":
+        await deleteSubscription(event);
+        break;
+    }
+  } catch (error) {
+    console.log("error ---> ", err.message);
+    res.send({ success: false });
+  }
 }
+
+async function updateSubscription(event) {
+  const subscription = event.data.object;
+  const stripe_customer_id = subscription.customer;
+  const subscription_status = subscription.status;
+  const price = subscription.items.data[0].price.id;
+  const { data: profile } = await supabase
+    .from("profile")
+    .select("*")
+    .eq("stripe_customer_id", stripe_customer_id)
+    .single();
+  if (profile) {
+    const updatedSubscription = {
+      subscription_status,
+      price,
+    };
+    await supabase
+      .from("profile")
+      .update(updatedSubscription)
+      .eq("stripe_customer_id", stripe_customer_id);
+  } else {
+    const customer = await stripe.customers.retrieve(stripe_customer_id);
+    const name = customer.name;
+    const email = customer.email;
+    const newProfile = {
+      name,
+      email,
+      stripe_customer_id,
+      subscription_status,
+      price,
+    };
+    await supabase.auth.admin.createUser({
+      email,
+      email_confirm: true,
+      user_metadata: newProfile,
+    });
+  }
+}
+
+async function deleteSubscription(event) {}
