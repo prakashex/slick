e52f815c36d7949c510d6e7c2af676453ad67c21
Prakash Kumar <prakashope@gmail.com>
2023-06-12 16:02:53 +0530

    login page

--- src/login/components/LoginForm.js
+++ src/login/components/LoginForm.js
@@ -1,1 +1,49 @@
-export default function LoginForm() {}
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useState } from "react";
+
+export default function LoginForm() {
+
+    const [error , setError] = useState('')
+    const [isloading , setIsLoading] = useState(false)
+  const supabaseClient = useSupabaseClient();
+
+  async function onSubmit(event) {
+    setIsLoading(true)
+    event.preventDefault();
+    const email = event.target.elements.email.value;
+    const { error } = await supabaseClient.auth.signInWithOtp({
+      email,
+      options: {
+        shouldCreateUser: false,
+        emailRedirectTo: window.location.origin,
+      },
+    });
+
+    console.log("error login form",error)
+    if(error){
+        setError(error.message)
+        setIsLoading(false)
+        console.log(error)
+    }else{
+        setError('')
+        setIsLoading(false)
+    }
+  }
+  return (
+    <form className="content-grid home-hero">
+      {error && (
+        <div className="danger" role="alert">
+            {error}
+        </div>
+      )}
+      <h1>welcome back</h1>
+      <section className="email-input">
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" autoComplete="email" />
+      </section>
+      <button type="submit" className="large-button">
+        <div className="large-button-text">Login</div>
+      </button>
+    </form>
+  );
+}
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,1 +1,30 @@
-export default function LoginPage() {}
+import Link from "next/link";
+import Logo from "src/core/components/Logo";
+import LoginForm from "src/login/components/LoginForm";
+import login from "../../public/assets/login.png"
+import Image from "next/image";
+
+export default function LoginPage() {
+    return (
+        <div className="grid-halves h-screen">
+            <div className="border-right bg-offwhite">
+                <div className="column-padding">
+                    <div className="tablet-centered">
+                        <Link href="/" className="logo-container">
+                            <Logo/>
+                        </Link>
+                        <LoginForm />
+                    </div>
+                </div>
+            </div>
+            <div className="bg-navy border-right">
+                <Image src={login} alt="login" className="callout-image" />
+            </div>
+        </div>
+    )
+}
+
+
+
+
+
