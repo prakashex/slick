69e7215ac13ac116bede93829536bd86a53486e9
Prakash Kumar <prakashope@gmail.com>
2023-06-12 19:22:35 +0530

    billing api , edit customer details

--- src/pages/api/manage-billing.js
+++ src/pages/api/manage-billing.js
@@ -1,1 +1,30 @@
-export default async function handler(req, res) {}
+import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SITE_URL } from "src/pricing/utils";
+
+export default async function handler(req, res) {
+  const supabaseServerClient = createServerComponentSupabaseClient({
+    req,
+    res,
+  });
+
+  const {
+    data: { user },
+  } = await supabaseServerClient.auth.getUser();
+
+  if (!user) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  const { data: profile } = await supabaseServerClient
+    .from("Profile")
+    .select("stripe_customer_id")
+    .eq("user_id", user.id)
+    .single();
+
+  const session = await stripe.billingPortal.sessions.create({
+    customer: profile.stripe_customer_id,
+    return_url: SITE_URL,
+  });
+
+  res.send({ url: session.url });
+}
