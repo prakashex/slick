6f7d4dc64b0c0159b5c1772a3b7662b11620b88b
Prakash Kumar <prakashope@gmail.com>
2023-06-13 15:32:12 +0530

    changed prod url

--- src/pricing/utils/index.js
+++ src/pricing/utils/index.js
@@ -1,3 +1,3 @@
 const production = process.env.NODE_ENV === "production"
 
-export const SITE_URL = production ? "prod-url" : "http://localhost:3000"
+export const SITE_URL = production ? "https://slick-prakashex.vercel.app" : "http://localhost:3000"
