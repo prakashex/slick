780241619f7f4c6e9386641b58fccfa586555084
Prakash <prakashope@gmail.com>
2023-06-13 16:07:28 +0530

    Update index.js

--- src/pricing/utils/index.js
+++ src/pricing/utils/index.js
@@ -1,3 +1,3 @@
 const production = process.env.NODE_ENV === "production"
 
-export const SITE_URL = production ? "slickit.vercel.app" : "http://localhost:3000"
+export const SITE_URL = production ? "https://slickit.vercel.app" : "http://localhost:3000"
